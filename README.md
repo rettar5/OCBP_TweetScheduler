9a5f9865bca5bea74aaead07202621a3cf2e6cbf
rettar5 <rettar.v@gmail.com>
2018-04-05 20:14:54 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TweetScheduler
+## Install
+
+```
+$ cd ./ODN-CLIENT
+$ cd ./src/plugins/batch
+$ git clone https://github.com/rettar5/OCPT_TweetScheduler.git tweetScheduler
+$ npm run load:build
+```
+
---
+++ tweetScheduler.ts
@@ -0,0 +1,153 @@
+import { Log } from "../../../odnUtils";
+import { AccountConfigs, AccountData } from "../../../configs/accountConfigs";
+import { OdnPlugins } from "../../../odnPlugins";
+import { OdnTweets } from "../../../odnTweets";
+
+export class TweetScheduler {
+  constructor(private accountData: AccountData, private nowDate: Date, private fullName: string) {}
+
+  /**
+   * プラグインのメイン処理を実行
+   *
+   * @param {(isProcessed?: boolean) => void} finish
+   */
+  run(finish: (isProcessed?: boolean) => void) {
+    const storedData = TweetScheduler.getStoredSchedule(this.fullName, this.accountData.userId);
+    const dateKey = TweetScheduler.getScheduleKey(this.nowDate);
+    if (storedData && storedData[dateKey]) {
+      for (const reservedNumber of Object.keys(storedData[dateKey])) {
+        const tweets = new OdnTweets(this.accountData);
+        tweets.text = storedData[dateKey][reservedNumber].message;
+        tweets.postTweet((isSuccess, error, tweetData, response) => {
+          if (isSuccess) {
+            TweetScheduler.deleteSchedule(this.accountData.userId, this.nowDate, parseInt(reservedNumber));
+          }
+        });
+      }
+    }
+
+    finish();
+  }
+
+  /**
+   * プラグインを実行するかどうか判定
+   *
+   * @param accountData
+   * @param nowDate
+   * @returns {boolean}
+   */
+  static isValid(accountData: AccountData, nowDate: Date): boolean {
+    // 毎分実行のため常にtrue
+    return true;
+  }
+
+  /**
+   * 保存済みのスケジュールを取得
+   *
+   * @param userId
+   * @returns {any|{}}
+   */
+  static getStoredSchedule(pluginFullName: string, userId: string): {string: {number: ScheduleData}} {
+    return OdnPlugins.getStoredData(pluginFullName, userId) || {};
+  }
+
+  /**
+   * スケジュールを保存
+   *
+   * @param userId
+   * @param data
+   */
+  static saveSchedule(pluginFullName: string, userId: string, data: {string: {number: ScheduleData}}) {
+    OdnPlugins.setStoredData(pluginFullName, userId, data);
+  }
+
+  /**
+   * ツイート投稿を予約
+   *
+   * @param userId
+   * @param date
+   * @param message
+   * @returns {number}
+   */
+  static setSchedule(userId: string, date: Date, message: string): number {
+    const pluginFullName = TweetSchedulerConstats.PLUGIN_FULL_NAME;
+    const storedData = TweetScheduler.getStoredSchedule(pluginFullName, userId);
+    const dateKey = TweetScheduler.getScheduleKey(date);
+    const reservedKey = (() => {
+      const reservedKeyList = storedData && storedData[dateKey] ? Object.keys(storedData[dateKey]) : null;
+      return reservedKeyList ? Math.max.apply(null, reservedKeyList) : 1;
+    })();
+    if (1 === reservedKey) {
+      storedData[dateKey] = {};
+    }
+    storedData[dateKey][reservedKey] = message;
+    TweetScheduler.saveSchedule(pluginFullName, userId, storedData);
+    return reservedKey;
+  }
+
+  /**
+   * 予約済みの投稿を削除
+   *
+   * @param userId
+   * @param date
+   * @param reservedKey
+   * @returns {boolean}
+   */
+  static deleteSchedule(userId: string, date: Date, reservedKey?: number): boolean {
+    let hasMessage = false;
+    const pluginFullName = TweetSchedulerConstats.PLUGIN_FULL_NAME;
+    const storedData = TweetScheduler.getStoredSchedule(pluginFullName, userId);
+    const dateKey = TweetScheduler.getScheduleKey(date);
+    if (storedData && storedData[dateKey]) {
+      if (reservedKey) {
+        if (storedData[dateKey][reservedKey]) {
+          delete storedData[dateKey][reservedKey];
+          hasMessage = true;
+        } else {
+          hasMessage = false;
+        }
+      } else {
+        delete storedData[dateKey];
+        hasMessage = true;
+      }
+    }
+
+    if (hasMessage) {
+      TweetScheduler.saveSchedule(pluginFullName, userId, storedData);
+    }
+    return hasMessage;
+  }
+
+  /**
+   * スケジュール保存用のIDを取得
+   *
+   * @param date
+   * @returns {string}
+   */
+  static getScheduleKey(date: Date): string {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    return year + month + day + "-" + hour + minute;
+  }
+}
+
+class ScheduleData {
+  date: Date;
+  message: string;
+
+  constructor(item?: any) {
+    if (item) {
+      for (const key of Object.keys(item)) {
+        this[key] = item[key];
+      }
+    }
+  }
+}
+
+namespace TweetSchedulerConstats {
+  export const PLUGIN_FULL_NAME = "PluginsBatchTweetScheduler";
+}
